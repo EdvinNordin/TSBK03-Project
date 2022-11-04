838e30b25fda63a5874a38199646fd96f5e9f301
EdvinNordin <nordin.edvin@gmail.com>
2022-11-05 00:32:32 +0100

    Mouse and random functions

--- Assets/Fluid.cs
+++ Assets/Fluid.cs
@@ -22,11 +22,11 @@ public class Fluid
     float[] Vx0;
     float[] Vy0;
 
-    public Fluid(float dt, float diffusion, float viscosity)
+    public Fluid(float dt, float diffusion, float viscosity,int iter, int N)
     {
-        this.N = 100;
-        this.iter = 4;
-        this.size = 100;
+        this.N = N;
+        this.iter = iter;
+        this.size = N;
 
         this.dt = dt;
         this.diff = diffusion;
@@ -230,253 +230,15 @@ public class Fluid
         x[IX(N - 1, N - 1, N)] = 0.5f * (x[IX(N - 2, N - 1, N)] + x[IX(N - 1, N - 2, N)]);
     }
 
-    public float renderD(int i)
+    public float getDensity(int i)
     {
-                //square (i,j) color from this.density[i];
-       return this.density[i];     
+        return this.density[i];
     }
 
-
-    // Start is called before the first frame update
-    void Start()
+    public float fadeDensity(int i, float amount)
     {
-        
+        this.density[i] -= amount;
+        return this.density[i];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
-/*
- * 
- * 
- using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-public class Fluid : MonoBehaviour
-{
-
-    GameObject[] points;
-    public GameObject prefab;
-
-    /*int size;
-    float dt;
-    float diff;
-    float visc ;
-
-    float[] s;
-    float[] density;
-
-    float[] Vx;
-    float[] Vy;
-
-    float[] Vx0;
-    float[] Vy0;*/
-/*
-
-public int N = 64;
-
-
-
-void Start()
-{
-    int size = 64;
-    float dt = 0.2f;
-    float diff = 0f;
-    float visc = 0.0001f;
-
-    float[] s = new float[N * N];
-    float[] density = new float[N * N];
-
-    float[] Vx = new float[N * N];
-    float[] Vy = new float[N * N];
-
-    float[] Vx0 = new float[N * N];
-    float[] Vy0 = new float[N * N];
-
-    GameObject[] points = new GameObject[N * N];
-    int temp = 0;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            temp = IX(i, j, N);
-            points[temp] = Instantiate(prefab, new Vector3(i, j, 0), Quaternion.identity);
-        }
-    }
-
-    /*int N = size;
-
-    //this.size = size;
-    this.dt = 0.2;
-    this.diff = 0;
-    this.visc = 0.000001;
-
-    float[] s = new float[N * N];
-    float[] density = new float[N * N];
-
-    float[] Vx = new float[N * N];
-    float[] Vy = new float[N * N];
-
-    float[] Vx0 = new float[N * N];
-    float[] Vy0 = new float[N * N];
-}
-
-
-int IX(int x, int y, int N)
-{
-    return x + y * N;
-}
-
-void addDensity(int x, int y, float amount, int N)
-{
-    int index = IX(x, y, N);
-    this.density[index] += amount;
-}
-
-void diffuse(int b, float[] x, float[] x0, float diff, float dt, int iter, int N)
-{
-    float a = dt * diff * (N - 2) * (N - 2);
-    lin_solve(b, x, x0, a, 1 + 6 * a, iter, N);
-}
-
-void lin_solve(int b, float[] x, float[] x0, float a, float c, int iter, int N)
-{
-    float cRecip = 1.0f / c;
-    for (int t = 0; t < iter; t++)
-    {
-        for (int j = 1; j < N - 1; j++)
-        {
-            for (int i = 1; i < N - 1; i++)
-            {
-                x[IX(i, j, N)] =
-                  (x0[IX(i, j, N)] +
-                    a *
-                      (x[IX(i + 1, j, N)] +
-                        x[IX(i - 1, j, N)] +
-                        x[IX(i, j + 1, N)] +
-                        x[IX(i, j - 1, N)])) *
-                  cRecip;
-            }
-        }
-        set_bnd(b, x, N);
-    }
-}
-
-void project(float[] velocX, float[] velocY, float[] p, float[] div, int iter, int N)
-{
-    for (int j = 1; j < N - 1; j++)
-    {
-        for (int i = 1; i < N - 1; i++)
-        {
-            div[IX(i, j, N)] = (-0.5f *
-                (velocX[IX(i + 1, j, N)] -
-                  velocX[IX(i - 1, j, N)] +
-                  velocY[IX(i, j + 1, N)] -
-                  velocY[IX(i, j - 1, N)])) / N;
-            p[IX(i, j, N)] = 0;
-        }
-    }
-
-    set_bnd(0, div, N);
-    set_bnd(0, p, N);
-    lin_solve(0, p, div, 1, 6, iter, N);
-
-    for (int j = 1; j < N - 1; j++)
-    {
-        for (int i = 1; i < N - 1; i++)
-        {
-            velocX[IX(i, j, N)] -= 0.5f * (p[IX(i + 1, j, N)] - p[IX(i - 1, j, N)]) * N;
-            velocY[IX(i, j, N)] -= 0.5f * (p[IX(i, j + 1, N)] - p[IX(i, j - 1, N)]) * N;
-        }
-    }
-
-    set_bnd(1, velocX, N);
-    set_bnd(2, velocY, N);
-
-}
-
-void advect(int b, float[] d, float[] d0, float[] velocX, float[] velocY, float dt, int N)
-{
-    float i0, i1, j0, j1;
-
-    float dtx = dt * (N - 2);
-    float dty = dt * (N - 2);
-
-    float s0, s1, t0, t1;
-    float tmp1, tmp2, tmp3, x, y;
-
-    float Nfloat = N - 2;
-    float ifloat, jfloat;
-    int i, j, k;
-
-    for (j = 1, jfloat = 1; j < N - 1; j++, jfloat++)
-    {
-        for (i = 1, ifloat = 1; i < N - 1; i++, ifloat++)
-        {
-            tmp1 = dtx * velocX[IX(i, j, N)];
-            tmp2 = dty * velocY[IX(i, j, N)];
-            x = ifloat - tmp1;
-            y = jfloat - tmp2;
-
-            if (x < 0.5) x = 0.5f;
-            if (x > Nfloat + 0.5) x = Nfloat + 0.5f;
-            i0 = Mathf.Floor(x);
-            i1 = i0 + 1.0f;
-            if (y < 0.5) y = 0.5f;
-            if (y > Nfloat + 0.5) y = Nfloat + 0.5f;
-            j0 = Mathf.Floor(y);
-            j1 = j0 + 1.0f;
-
-            s1 = x - i0;
-            s0 = 1.0f - s1;
-            t1 = y - j0;
-            t0 = 1.0f - t1;
-
-            int i0i = (int)i0;
-            int i1i = (int)i1;
-            int j0i = (int)j0;
-            int j1i = (int)j1;
-
-            d[IX(i, j, N)] =
-              s0 * (t0 * d0[IX(i0i, j0i, N)] + t1 * d0[IX(i0i, j1i, N)]) +
-              s1 * (t0 * d0[IX(i1i, j0i, N)] + t1 * d0[IX(i1i, j1i, N)]);
-        }
-    }
-
-    set_bnd(b, d, N);
-}
-
-void set_bnd(int b, float[] x, int N)
-{
-    for (int i = 1; i < N - 1; i++)
-    {
-        x[IX(i, 0, N)] = b == 2 ? -x[IX(i, 1, N)] : x[IX(i, 1, N)];
-        x[IX(i, N - 1, N)] = b == 2 ? -x[IX(i, N - 2, N)] : x[IX(i, N - 2, N)];
-    }
-    for (int j = 1; j < N - 1; j++)
-    {
-        x[IX(0, j, N)] = b == 1 ? -x[IX(1, j, N)] : x[IX(1, j, N)];
-        x[IX(N - 1, j, N)] = b == 1 ? -x[IX(N - 2, j, N)] : x[IX(N - 2, j, N)];
-    }
-
-    x[IX(0, 0, N)] = 0.5f * (x[IX(1, 0, N)] + x[IX(0, 1, N)]);
-    x[IX(0, N - 1, N)] = 0.5f * (x[IX(1, N - 1, N)] + x[IX(0, N - 2, N)]);
-    x[IX(N - 1, 0, N)] = 0.5f * (x[IX(N - 2, 0, N)] + x[IX(N - 1, 1, N)]);
-    x[IX(N - 1, N - 1, N)] = 0.5f * (x[IX(N - 2, N - 1, N)] + x[IX(N - 1, N - 2, N)]);
-}
-
-
-
-// Update is called once per frame
-void Update()
-{
-
-}
-}
-
- */
--- Assets/Points.cs
+++ Assets/Points.cs
@@ -9,38 +9,67 @@ public class Points : MonoBehaviour
     public GameObject prefab;
     Fluid fluid;
     int N = 100;
-    int dir = 0;
+    int iter = 0;
+    Vector3 mousePos = new Vector3(0,0,0);
+    float density, xPos, yPos;
+    float fadeAmount = 0f;
+    float prevX, prevY, randX, randY, temp = 0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        Camera.main.orthographicSize = N/2;
+        Camera.main.transform.position = new Vector3(0, 0, -10);
+        
         pointObjects = new List<GameObject>();
+
+        //dt, diffusion, viscosity
+        fluid = new Fluid(0.1f, 0.000000001f, 0.0001f, iter, N);
+        //fluid.addDensity(N / 2, N / 2, 700, N);
 
         for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < N; j++)
             {
-                pointObjects.Add(Instantiate(prefab, new Vector3(i,j,0), Quaternion.identity));
-                
+                temp = Mathf.PerlinNoise(i * 10f / (N + 1f), j * 10f / (N + 1f)) * 2 - 1;
+                Debug.Log(temp);
+                fluid.addDensity(i, j, 100*temp, N);
+
+                pointObjects.Add(Instantiate(prefab, new Vector3(i-N / 2, j-N / 2, 0), Quaternion.identity));
             }
         }
-        fluid = new Fluid(0.01f, 0.0f, 0.00001f);
-        
 
     }
     // Update is called once per frame
     void Update()
     {
-        fluid.addDensity(32, 32, 100, N);
-        dir++;
-        fluid.addVelocity(32, 32, dir, N-dir, N);
+        xPos = Mathf.Lerp(-N, N, Mathf.InverseLerp(0, 1100, Input.mousePosition.x)) + N / 2;
+        yPos = Mathf.Lerp(-N / 2, N / 2, Mathf.InverseLerp(0, 510, Input.mousePosition.y)) + N / 2;
+        //Debug.Log(xPos);
+
+        fluid.addVelocity((int)yPos, (int)xPos, yPos-prevY, xPos-prevX,  N);
         fluid.step();
         Color color = Color.white;
-        for (int i = 0; i < N * N; i++)
+        for (int i = 0; i < N; i++)
         {
-            float density = fluid.renderD(i);
-            color = new Color(density, density, density);
-            pointObjects[i].GetComponent<SpriteRenderer>().material.color = color;
+            for (int j = 0; j < N; j++)
+            {
+                randX = Mathf.PerlinNoise((float)i * 10 / ((float)N + 1f), (float)j * 10 * Time.time / ((float)N + 1f)) * 2 - 1;
+                randY = Mathf.PerlinNoise((float)i * 10 * Time.time / ((float)N + 1f), (float)j * 10 / ((float)N + 1f)) * 2 - 1;
+                //Debug.Log(Mathf.PerlinNoise((float)i * Time.time / ((float)N + 1f), (float)j * Time.time / ((float)N + 1f)) * 2 - 1);
+                //fluid.addVelocity(i, j, randX, randY, N);
+                density = fluid.getDensity(j + i * N);
+                density = fluid.fadeDensity(i, fadeAmount)/100;
+                color = new Color(density, density, density);
+                pointObjects[j + i * N].GetComponent<SpriteRenderer>().material.color = color;
+            }
         }
+        prevX = xPos;
+        prevY = yPos;
     }
 }
+/*
+        Debug.Log((mousePos.x - 1100 / 2) - (Input.mousePosition.y - 510 / 2));
+        fluid.addDensity((int)Input.mousePosition.y - 510 / 2,(int)Input.mousePosition.x - 1100 / 2,  1000, N);
+        fluid.addVelocity( (int)Input.mousePosition.y - 510 / 2,(int)Input.mousePosition.x - 1100 / 2, (mousePos.x - 1100/2) - (Input.mousePosition.y - 510 / 2), (Input.mousePosition.x - 1100 / 2) -(mousePos.x - 1100 / 2), N);*/
